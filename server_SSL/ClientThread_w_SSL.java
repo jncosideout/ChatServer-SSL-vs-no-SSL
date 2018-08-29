a717d2e5fd81663a470cabf065cd5a40d9043fc2
jncosideout <beatya4@gator.uhd.edu>
2018-08-29 15:33:05 -0500

    NOT THE SAME CLASS AS MASTER (no SSL version)
    This is the old version without SSL. The classes are different but the filenames are the same for comparison purposes.

--- server_SSL/ClientThread_w_SSL.java
+++ server_SSL/ClientThread_w_SSL.java
@@ -1,21 +1,18 @@
-// place this file the path such ends with: ChatServer_w_SSL/server/ClientThread.java
+// place this file the path such ends with: ChatServer/server/ClientThread.java
 
-package server_SSL;
+package server;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
-import javax.net.ssl.*;
-
-
 import java.util.Scanner;
 
-public class ClientThread_w_SSL implements Runnable {
-    private SSLSocket socket;
+public class ClientThread implements Runnable {
+    private Socket socket;
     private PrintWriter clientOut;
-    private ChatServer_w_SSL server;
+    private ChatServer server;
 
-    public ClientThread_w_SSL(ChatServer_w_SSL server, SSLSocket socket){
+    public ClientThread(ChatServer server, Socket socket){
         this.server = server;
         this.socket = socket;
     }
@@ -30,17 +27,14 @@ public class ClientThread_w_SSL implements Runnable {
             // setup
             this.clientOut = new PrintWriter(socket.getOutputStream(), false);
             Scanner in = new Scanner(socket.getInputStream());
-            
-            //setup handshake
-			socket.setEnabledCipherSuites(socket.getSupportedCipherSuites());
-			socket.startHandshake();
+
             // start communicating
             while(!socket.isClosed()){
                 if(in.hasNextLine()){
                     String input = in.nextLine();
                     // NOTE: if you want to check server can read input, uncomment next line and check server file console.
-                     System.out.println(input);
-                    for(ClientThread_w_SSL thatClient : server.getClients()){
+                    // System.out.println(input);
+                    for(ClientThread thatClient : server.getClients()){
                         PrintWriter thatClientOut = thatClient.getWriter();
                         if(thatClientOut != null){
                             thatClientOut.write(input + "\r\n");
@@ -51,8 +45,6 @@ public class ClientThread_w_SSL implements Runnable {
             }
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (Exception a) {
-        	a.printStackTrace();
         }
     }
 }
